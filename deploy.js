516f2a6aacf2761215ff97fc6935d931a0e0e156
dubisdev <dubisdev@gmail.com>
2022-12-02 12:46:21 +0100

    fix: deploy env variable

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const deployOAuthURL = process.env.DEPLOY_ENDPOINT_OAUTH;
+const deployOAuthURL = process.env.DEPLOY_ENDPOINT_SETTINGS;
 
 if (deployOAuthURL) await axios.get(deployOAuthURL);
 
