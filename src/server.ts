8c7b56dd17f0404776cfdc4b8f8d9f65bb5d5c22
dubisdev <dubisdev@gmail.com>
2022-12-02 13:12:12 +0100

    fix: handle body not being object

--- src/server.ts
+++ src/server.ts
@@ -46,7 +46,12 @@ function getRequestBody(req: IncomingMessage): Promise<DoistCardRequest> {
   return new Promise((resolve, reject) => {
     let body = '';
     req.on('data', chunk => body += chunk.toString());
-    req.on('end', () => resolve(JSON.parse(body)));
+    req.on('end', () => {
+      try {
+        const result = JSON.parse(body);
+        resolve(result);
+      } catch (err) { reject(err); }
+    });
     req.on('error', err => reject(err));
   });
 }
